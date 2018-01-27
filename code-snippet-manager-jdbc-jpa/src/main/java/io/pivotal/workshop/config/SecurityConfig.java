06f85e234d7eb2853ad03f27e47f569b58eeca5b
Soumil Vavikar <soumil.vavikar@gmail.com>
2018-01-27 14:45:56 +0530

    spring boot security

---
+++ code-snippet-manager-jdbc-jpa/src/main/java/io/pivotal/workshop/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package io.pivotal.workshop.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+// Configuring this class prevents the Spring Boot autoconfiguration
+// from happening.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	protected void configureUser(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication()
+				.withUser("user").password("pass").roles("USER")
+				.and()
+				.withUser("admin").password("password").roles("ADMIN");
+	}
+
+	// We do not want the default behavior of form authentication
+	// before HTTP Basic authentication we get
+	// from WebSecurityConfigurerAdapter.
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().fullyAuthenticated()
+				.and()
+				.httpBasic();
+	}
+}
